df9b790f2931dc795c3dd9d1cf7117629f58449b
jrm <minot.j@gmail.com>
2018-04-10 15:10:15 -0400

    more comments

--- rand_range.m
+++ rand_range.m
@@ -1,3 +1,15 @@
 function[num] = rand_range(a,b)
+% rand_range(a,b)
+%
+% return a random value within the range specified (a,b), drawn from a uniform
+% distribution
+% 
+% MANDATORY INPUTS: 
+%   a: min value in the range 
+%   b: max value in the range 
+%
+% OUTPUTS: 
+%   num: a random number from within the range (a,b)
+
 num = (b-a) .* rand()+a;
 end
--- rule3.m
+++ rule3.m
@@ -1,4 +1,19 @@
 function return_velocity = rule3(u_vec, v_vec, num_boids, i)
+% rule3(u_vec, v_vec, num_boids, i)
+%
+% calculate the update vector (velocity) to have boid i match the velocity
+% of the vectors 
+% 
+% MANDATORY INPUTS: 
+%   u_vec: vector of boid u velocities
+%   v_vec: vector of boid v velocities  
+%   num_boids: number of boids in our populations
+%   i: boid being examined
+%
+% OUTPUTS: 
+%   return_velocity: a vector of velocities based on matching boid i's
+%   velocity to mean velocity of flock. 
+
 
 percieved_velocity = zeros(2,1);
 
